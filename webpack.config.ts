a67fad05c56392a58bf92b49c52d0131ecf78116
AhmadEmadEldin <Ahmad-Emad@outlook.com>
2026-07-05 22:38:18 +0300

    Update webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -9,13 +9,24 @@ dotenv.config();
 const port = Number(process.env.PORT || process.env.CANVA_FRONTEND_PORT || 8081);
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY || "",
-  appId: process.env.VITE_FIREBASE_APP_ID || "",
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN || "",
-  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID || "",
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET || "",
+  apiKey: process.env.VITE_FIREBASE_API_KEY || process.env.FIREBASE_API_KEY || "",
+  appId: process.env.VITE_FIREBASE_APP_ID || process.env.FIREBASE_APP_ID || "",
+  authDomain:
+    process.env.VITE_FIREBASE_AUTH_DOMAIN || process.env.FIREBASE_AUTH_DOMAIN || "",
+  measurementId:
+    process.env.VITE_FIREBASE_MEASUREMENT_ID ||
+    process.env.FIREBASE_MEASUREMENT_ID ||
+    "",
+  messagingSenderId:
+    process.env.VITE_FIREBASE_MESSAGING_SENDER_ID ||
+    process.env.FIREBASE_MESSAGING_SENDER_ID ||
+    "",
+  projectId:
+    process.env.VITE_FIREBASE_PROJECT_ID || process.env.FIREBASE_PROJECT_ID || "",
+  storageBucket:
+    process.env.VITE_FIREBASE_STORAGE_BUCKET ||
+    process.env.FIREBASE_STORAGE_BUCKET ||
+    "",
 };
 
 const config: Configuration & { devServer?: DevServerConfiguration } = {
